a8aa1bff0eaea1186b1071a8bf73345426013297
kmfdm9000 <ka4nevlxa@gmail.com>
2021-08-04 01:35:45 +0300

    slider not prepared

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,8 @@
 $(function () {
     $('.top-slider__inner').slick({
         dots: false,
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/arrow-left.svg" alt="стрелка влево"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/icons/arrow-right.svg" alt="стрелка вправо"></button>',
         arrows: true
     })
 });
