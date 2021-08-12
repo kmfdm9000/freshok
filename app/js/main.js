caa2e7bd73657ffc7f2aa898fc9765b3ff54ba93
kmfdm9000 <ka4nevlxa@gmail.com>
2021-08-12 14:54:40 +0300

    main page done v1.0

--- app/js/main.js
+++ app/js/main.js
@@ -11,22 +11,24 @@ $(function () {
         infinite: true,
         slidesToShow: 6,
         slidesToScroll: 1,
-        autoplay: false,
-        autoplaySpeed: 2000,
+        autoplay: true,
+        autoplaySpeed: 5000,
         arrows: false
     })
     var containerEl1 = document.querySelector('[data-ref="mixer1"]');
     var containerEl2 = document.querySelector('[data-ref="mixer2"]');
-    var mixer1 = mixitup(containerEl1, {
+    var config = {
+        controls: {
+            scope: 'local'
+        },
         animation: {
             effectsOut: 'fade translateY(-100%)'
         }
-    });
-
-
-    var mixer2 = mixitup(containerEl2, {
-        animation: {
-            effectsOut: 'fade translateY(-100%)'
-        }
-    });
+    };
+    
+    var mixer1 = mixitup(containerEl1, config);
+    var mixer2 = mixitup(containerEl2, config);
 });
+
+
+ 
